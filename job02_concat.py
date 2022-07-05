5d7e8c4c42054f14e05435b6968c2e78efff22d3
yekwon <rkd5562@naver.com>
2022-07-05 17:00:01 +0900

    TFIDF 07

--- job02_concat.py
+++ job02_concat.py
@@ -5,21 +5,30 @@ import glob
 
 
 df = pd.DataFrame()
-data_paths = glob.glob('./crawling_data/MR_2017/*')
-# print(data_paths)
+data_paths = glob.glob('./CRO/*')
 
-
-for path in range(1, 38):
+for path in data_paths:
     df_temp = pd.read_csv(path)
     df_temp.dropna(inplace=True)
-    df_temp.drop_duplicates(inplace=True) #난값제거
-    df = pd.concat([df, df_temp], ignore_index=True) #중복제거
+    df_temp.drop_duplicates(inplace=True)
+    df = pd.concat([df, df_temp], ignore_index=True)
+
+# for path in range(1, 44):
+#     df_temp = pd.read_csv(path)
+#     df_temp.dropna(inplace=True)
+#     df_temp.drop_duplicates(inplace=True) #난값제거
+#     df = pd.concat([df, df_temp], ignore_index=True) #중복제거
 
 #합친다음 중복이 있을수도있으니 ,중복제거 한번더
 
+# df.drop_duplicates(inplace=True)
+# df.info()
+# my_year = 2019
+# df.to_csv('./crawling_data/reviews_{}.csv'.format(my_year))
+# #팀원은 합쳐진 파일 깃 > 에드 > 방장이 확인 pr
+
 df.drop_duplicates(inplace=True)
 df.info()
-my_year = 2017
-df.to_csv('./crawling_data/reviews_{}.csv'.format(my_year))
-#팀원은 합쳐진 파일 깃 > 에드 > 방장이 확인 pr
-
+# my_year = 2019
+# df.to_csv('./crawling_data/reviews_{}.csv'.format(my_year), index=False)
+df.to_csv('./crawling_data/reviews_2017_2022.csv', index=False)
--- job03_preprocessing_01.py
+++ job03_preprocessing_01.py
@@ -1,50 +1,106 @@
-import pandas as pd
-form konlypy.tag import Okt
 
-df=pd.read_csv('./crawling_data/reviews_2017.csv')
+
+from unittest.mock import inplace
+
+import pandas as pd
+import stopwords as stopwords
+from konlpy.tag import Okt
+import re
+
+df = pd.read_csv('./crawling_data/reviews_2018.csv')
 df.info()
 
-#형태소분리 먼저
 okt = Okt()
 
-
-
-#첫번쨰 전처리 한글남기고 다 버리기.  > 불용어 버리기 (스탑워드) ,한글자버리기
-#전처리2에서는 형태소 나누고 등등
-
-df_stopword = pd.read_csv('./crawling_data/stopwords.csv')
+df_stopwords = pd.read_csv('./crawling_data/stopwords.csv')
 stopwords = list(df_stopwords['stopword'])
 
-token = okt.pos(df.reviews[0], stem=True)
-print(token)
-exit()
 
 cleaned_sentences = []
 for review in df.reviews:
-    review = re.sub('[^가-힣 ]', ' ', review)  #^이 있어야 이거 뒤에꺼 뺴고 나머지 지우기임, ^없으면 가-힣 다지움
-    #형태소분리하기
-    token = okt.pos(review, stem=True) # morphs 몹스 형태소 짤라주는애 . 기존사용함, pos는 짤라준후 ,형태소의 품사까지 알려줌. 딕션어리형태임(튜플로)
+    review = re.sub('[^가-힣 ]', ' ', review)
+    token = okt.pos(review, stem=True)
 
-    #동사 명사 부사만 남기기 > 데이터프레임 먼저 만들기 , 형태소와 품사 2가지로 됨
-    df_token = pd.DataFrame(token, columns=['word','class'])
-    df_token = pd_token[(df_token['class']=='Noun')
-                        (df_token['class']=='Verb')
-                        (df_token['class']=='Adjective')]
-    #부용어제거하기
+    df_token = pd.DataFrame(token, columns=['word', 'class'])
+    df_token = df_token[(df_token['class'] == 'Noun') |
+                        (df_token['class'] == 'Verb') |
+                        (df_token['class'] == 'Adjective')]
     words = []
     for word in df_token.word:
         if len(word) > 1:
             if word not in stopwords:
-                words.append(word) #워드에다가 넣기
+                words.append(word)
+    cleaned_sentence = ' '.join(words)
+    cleaned_sentences.append(cleaned_sentence)
 
-
-    #한문장으로 팝치기, 형태소들을 띄어쓰기 기준으로 합치기
-    cleaned_sentence = ' '.join(words) #한글만, 명사 동사 수식어만, 한글자 짜리 없음 ,스톱워드제거상태로 다시 이어 붙이기
-    #아까 만들어 놓은 빈 리스트에 넣기
-    cleaned_sentence.append(cleaned_sentence)
 df['cleaned_sentences'] = cleaned_sentences
-df = df['title', 'cleaned_sentences']
+df = df[['title', 'cleaned_sentences']]
 df.dropna(inplace=True)
 
-df.to_csv('./crawling_data/cleaned_review_2017.csv',index=False)
+df.to_csv('./crawling_data/cleaned_review_2018.csv', index=False)
 df.info()
+
+
+
+# from unittest.mock import inplace
+#
+# import pandas as pd
+# import stopwords as stopwords
+# from konlpy.tag import Okt
+# import re
+#
+# df=pd.read_csv('./crawling_data/reviews_2020.csv')
+# df.info()
+#
+# #형태소분리 먼저
+# okt = Okt()
+#
+#
+#
+# #첫번쨰 전처리 한글남기고 다 버리기.  > 불용어 버리기 (스탑워드) ,한글자버리기
+# #전처리2에서는 형태소 나누고 등등
+#
+# df_stopword = pd.read_csv('./crawling_data/stopwords.csv')
+# stopwords = list(df_stopwords['stopword'])
+#
+# token = okt.pos(df.reviews[0], stem=True)
+# print(token)
+# exit()
+#
+# cleaned_sentences = []
+# for review in df.reviews:
+#     count += 1
+#     if count % 10 == 0:
+#         print('.', end='')
+#     if count % 100 == 0:
+#         print()
+#
+#
+#
+#     review = re.sub('[^가-힣 ]', ' ', review)  #^이 있어야 이거 뒤에꺼 뺴고 나머지 지우기임, ^없으면 가-힣 다지움
+#     #형태소분리하기
+#     token = okt.pos(review, stem=True) # morphs 몹스 형태소 짤라주는애 . 기존사용함, pos는 짤라준후 ,형태소의 품사까지 알려줌. 딕션어리형태임(튜플로)
+#
+#     #동사 명사 부사만 남기기 > 데이터프레임 먼저 만들기 , 형태소와 품사 2가지로 됨
+#     df_token = pd.DataFrame(token, columns=['word','class'])
+#     df_token = pd_token[(df_token['class']=='Noun')
+#                         (df_token['class']=='Verb')
+#                         (df_token['class']=='Adjective')]
+#     #부용어제거하기
+#     words = []
+#     for word in df_token.word:
+#         if len(word) > 1:
+#             if word not in stopwords:
+#                 words.append(word) #워드에다가 넣기
+#
+#
+#     #한문장으로 팝치기, 형태소들을 띄어쓰기 기준으로 합치기
+#     cleaned_sentence = ' '.join(words) #한글만, 명사 동사 수식어만, 한글자 짜리 없음 ,스톱워드제거상태로 다시 이어 붙이기
+#     #아까 만들어 놓은 빈 리스트에 넣기
+#     cleaned_sentence.append(cleaned_sentence)
+# df['cleaned_sentences'] = cleaned_sentences
+# df = df['title', 'cleaned_sentences']
+# df.dropna(inplace=True)
+#
+# df.to_csv('./crawling_data/cleaned_review_2020.csv',index=False)
+# df.info()
